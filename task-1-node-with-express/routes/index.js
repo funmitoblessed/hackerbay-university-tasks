9f6a0aa7e2907e5743672f787c9506ed87f5c20f
Funmito <funmito.odefemi@outlook.com>
2018-08-23 23:25:06 +0000

    Create GET API

--- task-1-node-with-express/routes/index.js
+++ task-1-node-with-express/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
+/* GET default page and return {status: success} */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { status: 'success' });
 });
 
 module.exports = router;
