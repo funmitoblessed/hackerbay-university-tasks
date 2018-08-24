aa4f01d5bb78ebe99907936fbc53ca97dc0486d0
Funmito <funmito.odefemi@outlook.com>
2018-08-24 01:07:08 +0000

    Update code with handlebars templating engine

--- task-1-node-with-express/routes/index.js
+++ task-1-node-with-express/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET default page and return {status: success} */
 router.get('/', function(req, res, next) {
-    res.render('index', { status: 'success' });
+    res.render('index', { title: 'Working with Handlebars', status: 'success' });
 });
 
 module.exports = router;
