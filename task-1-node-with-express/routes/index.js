cb397b1193e8ae43bd14b3ed5e8f0392dafefa3f
Funmito <funmito.odefemi@outlook.com>
2018-08-27 23:39:50 +0000

    Add GET and POST  requests

--- task-1-node-with-express/routes/index.js
+++ task-1-node-with-express/routes/index.js
@@ -1,10 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 
 /* GET default page and return {status: success} */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Working with Handlebars', status: 'success' });
 });
+
+// Create POST API
+router.post('/data', function(req, res, next) {
+    // var data = req.body.data;
+    // var anyString = req.body.anyString;
+    res.redirect(200, '/data')
+});
+
+// GET Request to get the data saved with POST
+router.get('/data', function(req, res, next) {
+    res.render('data', { title: 'Get Request', data: 'My name is Funmito' });
+});
+
+
 
 module.exports = router;
