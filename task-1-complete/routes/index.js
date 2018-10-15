05d2abe2cfbedd481d83ce8f9fb0925ac9deb823
Funmito <funmito.odefemi@outlook.com>
2018-10-15 04:28:37 +0100

    Add task-1 in own folder

---
+++ task-1-complete/routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+
+/* GET default page and return {status: success} */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'First APIs', status: 'success' });
+});
+
+// Create POST API and return data from body
+router.post('/data', function(req, res, next) {
+    var data = req.body.data;
+    res.json({ data: data });
+});
+
+module.exports = router;
