fc546d4bbbc883bc1c22fbd21b6ff510976c653c
Funmito <funmito.odefemi@outlook.com>
2018-10-20 01:10:23 +0100

    Update code for defining models

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var hbs = require('express-handlebars');
-
 var index = require('./routes/index');
 
 var app = express();
+
 
 // view engine setup
 app.engine('hbs', hbs({ extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/' })); // specify extension of files and default layout folder
@@ -40,3 +40,64 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+// Sequelize
+const Sequelize = require('sequelize');
+
+// Information for sequelize to connect to database
+const connection = new Sequelize('blessed', 'blessed', 'mvfDB918', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+// create and define model using your connection information
+// model takes two arguments - the name of the model and an object representing its properties
+let Article = connection.define('article', {
+    // the value of each property must be the data type it represents
+    slug: {
+        type: Sequelize.STRING,
+        // tell sequelize to not autogenerate an id
+        primaryKey: true
+    },
+    title: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    body: {
+        type: Sequelize.TEXT,
+        defaultValue: 'Coming Soon...'
+    }
+}, {
+    // disable timestamps option
+    timestamps: false
+});
+
+// Synchronize database by calling the sync function on the connection object
+// connection.sync().then(function() {
+//     // insert new record into our table 
+//     Article.create({
+//         title: 'My First Article',
+//         body: 'This is going to be our first post using a Relational Database (posgres). We are using Sequelize as our ORM'
+//     });
+// });
+
+
+// retrieving a record from our database using its id when you have it
+// connection.sync().then(function() {
+//     Article.findById(6).then(function(article) {
+//         console.log(article.dataValues);
+//     })
+// });
+
+// retrieving all records from the database when you don't have an id
+connection.sync({
+    // delete all database entry
+    // force: true,
+    logging: console.log
+}).then(function() {
+    // Article.findAll().then(function(articles) {
+    //     console.log(articles.length);
+    // })
+});
