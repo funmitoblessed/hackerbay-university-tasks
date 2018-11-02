7427ee13aef62366a25791d38def46218d30b893
Oluwafunmito Blessed <funmito.odefemi@outlook.com>
2018-11-02 16:23:26 +0100

    feat: add signup functionality

--- task-2/models/user.js
+++ task-2/models/user.js
@@ -22,29 +22,35 @@ let User = sequelize.define('user', {
 sequelize.sync();
 
 // User functions
-module.exports.createUser = function(newUser, callback) {
-    bcrypt.genSalt(10, function(err, salt) {
-        bcrypt.hash(newUser.password, salt, function(err, hash) {
-            newUser.password = hash;
-            newUser.save(callback);
-        });
-    });
-}
 
-module.exports.getUserByEmail = function(email, callback) {
-    let query = { email: email };
-    User.findOne(query, callback);
-}
+User.beforeCreate((user, options) => {
+    let salt = bcrypt.genSaltSync(10);
+    let hash = bcrypt.hashSync(user.password, salt);
+    return user.password = hash;
+});
+// module.exports.createUser = function(newUser, callback) {
+//     bcrypt.genSalt(10, function(err, salt) {
+//         bcrypt.hash(newUser.password, salt, function(err, hash) {
+//             newUser.password = hash;
+//             newUser.save(callback);
+//         });
+//     });
+// }
 
-module.exports.getUserById = function(id, callback) {
-    User.findById(id, callback);
-}
+// module.exports.getUserByEmail = function(email, callback) {
+//     let query = { email: email };
+//     User.findOne(query, callback);
+// }
 
-module.exports.comparePassword = function(candidatePassword, hash, callback) {
-    bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
-        if (err) throw err;
-        callback(null, isMatch);
-    });
-}
+// module.exports.getUserById = function(id, callback) {
+//     User.findById(id, callback);
+// }
+
+// module.exports.comparePassword = function(candidatePassword, hash, callback) {
+//     bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+//         if (err) throw err;
+//         callback(null, isMatch);
+//     });
+// }
 
 module.exports = User;
--- task-2/routes/user.js
+++ task-2/routes/user.js
@@ -4,145 +4,77 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-''
-
 
 // Get user model
 const User = require('../models/user');
 
-// User Signup
+// Render User Signup
 router.get('/signup', function(req, res) {
     res.render('signup');
 });
 
-
-// Login
+// Render User Login
 router.get('/login', function(req, res) {
     res.render('login');
 });
 
 
-// Register User
-router.post('/signup', function(req, res) {
-    let email = req.body.email;
-    let password = req.body.password;
-    let password2 = req.body.password2;
+// Register new User 
+router.post('/signup', (req, res) => {
+    let newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        password2: req.body.password2
+    }
 
-    console.log(email, password);
-
-    // Validation
-    req.checkBody('email', 'Email is required').notEmpty();
-    req.checkBody('email', 'Email is not valid').isEmail();
-    req.checkBody('password', 'Password is required').notEmpty();
-    req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
-
-    let errors = req.validationErrors();
-
-    if (errors) {
-        res.render('signup', {
-            errors: errors
-        });
-    } else {
-        let newUser = new User({
-            email: email,
-            password: password
-        });
-
-        User.createUser(newUser, function(err, user) {
-            if (err) throw err;
-            console.log(user);
-        });
-
-        req.flash('success_msg', 'You are registered and can now login');
-
-        res.redirect('/user/login');
-    };
-
-});
-
-
-passport.use(new LocalStrategy(
-    function(email, password, done) {
-        User.getUserByEmail(email, function(err, user) {
-            if (err) throw err;
-            if (!user) {
-                return done(null, false, { message: 'Unknown User' });
+    User.findOne({ where: { email: newUser.email } })
+        .then(user => {
+            if (user) return res.status(400).json({ msg: 'User already exists' })
+            else {
+                User.create(newUser)
+                    .then(user => {
+                        let payload = {
+                            email: user.email,
+                            password: user.password
+                        };
+                        jwt.sign(payload, 'secret', { expiresIn: '1h' }, (err, token) => {
+                            res.json({ session: token })
+                        });
+                    })
             }
-
-            User.comparePassword(password, user.password, function(err, isMatch) {
-                if (err) throw err;
-                if (isMatch) {
-                    return done(null, user);
-                } else {
-                    return done(null, false, { message: 'Invalid password' });
-                }
-            });
-        });
-    }));
-
-router.post('/login',
-    passport.authenticate('local', { successRedirect: '/', failureRedirect: '/user/login', failureFlash: true }),
-    function(req, res) {
-        res.redirect('/');
-    });
-
-router.get('/logout', function(req, res) {
-    req.logout();
-
-    req.flash('success_msg', 'You are logged out');
-
-    res.redirect('/user/login');
+        })
+        .catch(err => res.status(401).json(err));
 });
 
+router.post('/login', (req, res) => {
+    let newUser = {
+        email: req.body.email,
+        password: req.body.password
+    }
 
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-});
+    User.findOne({ where: { email: newUser.email } })
+        .then(user => {
+            if (user) {
+                bcrypt.compareSync(newUser.password, user.password)
+                    .then(isMatch => {
+                        if (isMatch) {
+                            let payload = { email: user.email, password: user.password }
+                            jwt.sign(payload, 'secret', { expiresIn: '1h' }, (err, token) => {
+                                res.json({ session: token })
+                            })
+                        } else {
+                            res.status(401).json({ error: 'Wrong Password' })
+                        }
+                    })
+            } else {
+                res.status(404).json({ error: 'User does not exist.' })
+            }
+        })
+        .catch(err => res.status(401).json(err));
+})
 
-passport.deserializeUser(function(id, done) {
-    User.getUserById(id, function(err, user) {
-        done(err, user);
-    });
-});
+
+
 
 
 module.exports = router;
-
-// if (errors) {
-//     res.render('signup', {
-//         errors: errors
-//     });
-// } else {
-//     //checking for email and username are already taken
-//     User.findOne({
-//         email: {
-//             "$regex": "^" + email + "\\b",
-//             "$options": "i"
-//         }
-//     }, function(err, user) {
-//         User.findOne({
-//             email: {
-//                 "$regex": "^" + email + "\\b",
-//                 "$options": "i"
-//             }
-//         }, function(err, mail) {
-//             if (user || mail) {
-//                 res.render('signup', {
-//                     user: user,
-//                     mail: mail
-//                 });
-//             } else {
-//                 let newUser = new User({
-//                     email: email,
-//                     password: password
-//                 });
-//                 User.createUser(newUser, function(err, user) {
-//                     if (err) throw err;
-//                     console.log(user);
-//                 });
-//                 req.flash('success_msg', 'You are registered and can now login');
-//                 res.redirect('/user/login');
-//             }
-//         });
-//     });
-// }
